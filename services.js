5adc16486661f1b98790bbcb940dab0043390719
Brad Butterfield <butters5789@gmail.com>
2016-05-06 10:27:36 -0600

    changed http to https

--- services.js
+++ services.js
@@ -4,10 +4,10 @@ app.service('_imdb', ['$http', function($http) {
     return {
         findMovie: function(movie) {
             var movieTitle = movie.toLowerCase().split(' ').join('+');
-            return $http.get('http://www.omdbapi.com/?s=' + movieTitle + '&r=json');
+            return $http.get('https://www.omdbapi.com/?s=' + movieTitle + '&r=json');
         },
         grabMovie: function(imdbID) {
-            return $http.get('http://www.omdbapi.com/?i=' + imdbID + '&plot=short&r=json');
+            return $http.get('https://www.omdbapi.com/?i=' + imdbID + '&plot=short&r=json');
         }
     }
 }]);
